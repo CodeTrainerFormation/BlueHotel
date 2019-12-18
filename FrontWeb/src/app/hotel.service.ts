05e4a7b596e2e91d80d620129bd0a687595beee0
Nab <noreply@codetrainer.fr>
2019-12-18 16:30:12 +0100

    post/put/delete added on service [Working]

--- FrontWeb/src/app/hotel.service.ts
+++ FrontWeb/src/app/hotel.service.ts
@@ -10,6 +10,9 @@ import { Hotel } from './model/hotel';
 export class HotelService {
 
   private apiUrl = 'https://localhost:44390/api/hotels';
+  private httpOptions = {
+    headers: new HttpHeaders({'content-type': 'application/json'})
+  };
 
   constructor(
     private http: HttpClient) { }
@@ -20,6 +23,22 @@ export class HotelService {
 
   getHotel(hotelId: number) : Observable<Hotel>{
     return this.http.get<Hotel>(`${this.apiUrl}/${hotelId}`);
+  }
+
+  addHotel(hotel: Hotel) : Observable<Hotel>{
+    return this.http.post<Hotel>(this.apiUrl, hotel, this.httpOptions);
+  }
+
+  updateHotel(hotel: Hotel) : Observable<any>{
+    const url = `${this.apiUrl}/${hotel.hotelId}`;
+    return this.http.put<Hotel>(url, hotel, this.httpOptions);
+  }
+
+  deleteHotel(hotel: Hotel | number) : Observable<Hotel>{
+    const id: number = typeof hotel === 'number' ? hotel : hotel.hotelId;
+    const url = `${this.apiUrl}/${id}`;
+    
+    return this.http.delete<Hotel>(url);
   }
 
 }
